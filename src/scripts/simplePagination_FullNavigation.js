2cccdb54ee684901563600c2139731c99cb39629
Mike Ludemann <mike-ludemann@hotmail.de>
2018-10-25 15:19:23 +0200

    Click event for show and hide pagination navigation and update index.html

--- src/scripts/simplePagination_FullNavigation.js
+++ src/scripts/simplePagination_FullNavigation.js
@@ -1,29 +1,29 @@
 var showCount = 5,
-    countElements = $('#list').children('li').size(),
+    countElements = $('#list').children('div').size(),
     countSites = Math.ceil(countElements / showCount),
     lastSite = countSites - 1;
 
-$(document).ready(function() {
+$(document).ready(function () {
 
     $('body').append('<div class="controls"></div><input id="current_page" type="hidden"><input id="showCount" type="hidden">');
     $('#current_page').val(0);
     $('#showCount').val(showCount);
 
-    var navigation_html = '<a class="prev" onclick="first()">First</a>';
-        navigation_html += '<a class="prev" onclick="previous()">Prev</a>';
-          
+    var navigation_html = '<a id="first" onclick="first()">First</a>';
+    navigation_html += '<a id="prev" onclick="previous()">Prev</a>';
+
     var actualLink = 0;
 
     while (countSites > actualLink) {
-        
-        navigation_html += '<a class="page" onclick="goToPage(' + actualLink + ')" longdesc="' + actualLink + '">' + (actualLink + 1) + '</a>';
-        
+
+        navigation_html += '<a id="site-' + (actualLink + 1) + '" class="page" onclick="goToPage(' + actualLink + ')" longdesc="' + actualLink + '">' + (actualLink + 1) + '</a>';
+
         actualLink++;
 
     }
 
-    navigation_html += '<a class="next" onclick="next()">Next</a>';
-    navigation_html += '<a class="next" onclick="last()">Last</a>';
+    navigation_html += '<a id="next" onclick="next()">Next</a>';
+    navigation_html += '<a id="last" onclick="last()">Last</a>';
 
     $('.controls').html(navigation_html);
     $('.controls .page:first').addClass('active');
@@ -33,7 +33,65 @@ $(document).ready(function() {
 
 });
 
+$(document).ready(function () {
+    
+    $("#first").click(function () {
+        $("#first").hide();
+        $("#prev").hide();
+        $("#last").show();
+        $("#next").show();
+    });
 
+    $("#last").click(function () {
+        $("#last").hide();
+        $("#next").hide();
+        $("#first").show();
+        $("#prev").show();
+    });
+
+    $("#site-1").click(function () {
+        $("#first").hide();
+        $("#prev").hide();
+        $("#last").show();
+        $("#next").show();
+    });
+
+    $("#site-" + countSites).click(function () {
+        $("#last").hide();
+        $("#next").hide();
+        $("#first").show();
+        $("#prev").show();
+    });
+
+    $("#prev").click(function () {
+        if ($("#site-1").hasClass("active")) {
+            $("#first").hide();
+            $("#prev").hide();
+            $("#last").show();
+            $("#next").show();
+        } else {
+            $("#first").show();
+            $("#prev").show();
+            $("#last").hide();
+            $("#next").hide();
+        }
+    });
+
+    $("#next").click(function () {
+        if ($("#site-" + countSites).hasClass("active")) {
+            $("#last").hide();
+            $("#next").hide();
+            $("#first").show();
+            $("#prev").show();
+        } else {
+            $("#last").show();
+            $("#next").show();
+            $("#first").hide();
+            $("#prev").hide();
+        }
+    });
+
+});
 
 function goToPage(page_num) {
 
@@ -59,7 +117,7 @@ function first() {
 function previous() {
 
     previous_Page = parseInt($('#current_page').val(), 0) - 1;
-    
+
     if ($('.active').prev('.page').length == true) {
 
         goToPage(previous_Page);
@@ -71,7 +129,7 @@ function previous() {
 function next() {
 
     next_Page = parseInt($('#current_page').val(), 0) + 1;
-    
+
     if ($('.active').next('.page').length == true) {
 
         goToPage(next_Page);
--- src/scripts/simplePagination_SmallNavigation.js
+++ src/scripts/simplePagination_SmallNavigation.js
@@ -1,5 +1,5 @@
 var showCount = 5,
-    countElements = $('#list').children('li').size(),
+    countElements = $('#list').children('div').size(),
     countSites = Math.ceil(countElements / showCount),
     lastSite = countSites - 1,
     index = 1;
@@ -10,21 +10,22 @@ $(document).ready(function () {
     $('#current_page').val(0);
     $('#showCount').val(showCount);
 
-    var navigation_html = '<a class="prev" onclick="first()">First</a>';
-    navigation_html += '<a class="prev" onclick="previous()">Prev</a>';
+    var navigation_html = '<a id="first" onclick="first()">First</a>';
+    navigation_html += '<a id="prev" onclick="previous()">Prev</a>';
 
     var current_link = 0;
 
     while (countSites > current_link) {
 
-        navigation_html += '<a style="display: none" class="page" onclick="goToSite(' + current_link + ')" longdesc="' + current_link + '">' + (current_link + 1) + '</a>';
-        
+        navigation_html += '<a id="site-' + (current_link + 1) + '" style="display: none" class="page" onclick="goToSite(' + current_link + ')" longdesc="' + current_link + '">' + (current_link + 1) + '</a>';
+
         current_link++;
 
     }
+    
     navigation_html += '<span class="index">' + (index) + '</span><span> / </span><span class="lastIndex">' + countSites + '</span>';
-    navigation_html += '<a class="next" onclick="next()">Next</a>';
-    navigation_html += '<a class="next" onclick="last()">Last</a>';
+    navigation_html += '<a id="next" onclick="next()">Next</a>';
+    navigation_html += '<a id="last" onclick="last()">Last</a>';
 
     $('.controls').html(navigation_html);
     $('.controls .page:first').addClass('active');
@@ -34,7 +35,66 @@ $(document).ready(function () {
 
 });
 
+$(document).ready(function () {
 
+    $("#first").click(function () {
+
+        $("#first").hide();
+        $("#prev").hide();
+        $("#last").show();
+        $("#next").show();
+
+    });
+
+    $("#last").click(function () {
+
+        $("#last").hide();
+        $("#next").hide();
+        $("#first").show();
+        $("#prev").show();
+
+    });
+
+    $("#prev").click(function () {
+
+        if ($("#site-1").hasClass("active")) {
+
+            $("#first").hide();
+            $("#prev").hide();
+            $("#last").show();
+            $("#next").show();
+
+        } else {
+
+            $("#first").show();
+            $("#prev").show();
+            $("#last").show();
+            $("#next").show();
+        }
+
+    });
+
+    $("#next").click(function () {
+
+        if ($("#site-" + countSites).hasClass("active")) {
+
+            $("#last").hide();
+            $("#next").hide();
+            $("#first").show();
+            $("#prev").show();
+
+        } else {
+
+            $("#last").show();
+            $("#next").show();
+            $("#first").show();
+            $("#prev").show();
+
+        }
+
+    });
+
+});
 
 function goToSite(page_num) {
 
@@ -42,7 +102,7 @@ function goToSite(page_num) {
 
     starts = page_num * showCount;
 
-    ends = start + showCount;
+    ends = starts + showCount;
 
     $('#list').children().css('display', 'none').slice(starts, ends).css('display', 'block');
 
@@ -55,16 +115,15 @@ function first() {
 
     goToSite(0);
     index = 1;
-    
-    $(".index").html(index);
 
+    $(".index").html(index);
 
 }
 
 function previous() {
 
     previousPage = parseInt($('#current_page').val(), 0) - 1;
-    
+
     if ($('.active').prev('.page').length == true) {
 
         goToSite(previousPage);
@@ -87,11 +146,11 @@ function previous() {
 
 function next() {
 
-    nextPage = parseInt($('#current_page').val(), 0) + 1;
-    
+    previousPage = parseInt($('#current_page').val(), 0) + 1;
+
     if ($('.active').next('.page').length == true) {
 
-        goToSite(nextPage);
+        goToSite(previousPage);
 
         if (index >= countSites && index == (current_link + 1)) {
 
